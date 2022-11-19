28a53a530161b1202dea5ee2eee6a044b69e6b87
Rafael Londoño <rafael.920427@gmail.com>
2022-11-19 18:49:31 -0500

    cargando pagina en GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FrontMascotas';
+  myimage:string="assets/img/logo-petland.png"
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
+import { FooterComponent } from './plantilla/footer/footer.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent,
+    FooterComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
